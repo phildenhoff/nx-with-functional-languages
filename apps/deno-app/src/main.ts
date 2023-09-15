726e4d9374aa9a024566cb58392e56b7806c0d70
Phil Denhoff <phil@denhoff.ca>
2023-09-14 20:37:58 -0700

    feat: deno app uses Civet library

--- apps/deno-app/src/main.ts
+++ apps/deno-app/src/main.ts
@@ -1,4 +1,5 @@
 import { Application, Router } from 'https://deno.land/x/oak@v11.1.0/mod.ts';
+import { Currency, Money } from '@utils/civet/finance';
 
 const router = new Router();
 
@@ -7,7 +8,13 @@ router.get('/', (ctx) => {
 });
 
 router.get('/api', (ctx) => {
-  ctx.response.body = { message: 'Hello deno-app' };
+  const ten = Money.new(10, Currency.CAD);
+  const twenty = Money.new(20, Currency.CAD);
+  const result = Money.sum([ten, twenty]);
+  console.log(Object.keys(result));
+  ctx.response.body = {
+    message: `Hello deno-app. You owe me $${result.toString()}.`,
+  };
   ctx.response.type = 'text/json';
 });
 
