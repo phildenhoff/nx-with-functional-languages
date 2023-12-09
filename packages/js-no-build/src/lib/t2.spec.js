ac48b8b272fa2b88c0f437380cd5d00ae78254e2
Phil Denhoff <phil@denhoff.ca>
2023-12-09 01:07:54 -0800

    import vitest

--- packages/js-no-build/src/lib/t2.spec.js
+++ packages/js-no-build/src/lib/t2.spec.js
@@ -1,4 +1,6 @@
+import { describe, it, expect } from 'vitest';
 import { makeOnce } from './t2';
+
 describe('makeOnce', () => {
   it('must return `once`', () => {
     expect(makeOnce()).toEqual('once');
